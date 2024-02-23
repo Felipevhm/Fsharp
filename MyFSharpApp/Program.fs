2bf6b5944135e20996906cbb95b746a8faa8673c
Felipe <fvhmanhaes@gmail.com>
2024-02-23 11:59:16 -0300

    added solved exercises | functions / boolean logic / string manipulation / if-then-else

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,28 @@
+﻿module CarsAssemble
+
+let successRate (speed: int): float =
+    let output = 
+            if speed = 0 then 
+                    0.0
+            elif (speed >= 1) && (speed <= 4) then
+                    1.0
+            elif (speed >= 5) && (speed <= 8) then
+                    0.9
+            elif speed = 9 then
+                    0.8
+            else 
+                0.77
+    output      
+
+let productionRatePerHour (speed: int): float =
+    //failwith "Please implement the 'productionRatePerHour' function"
+    let floatSpeed = float speed
+    let output = floatSpeed*221.0*(successRate speed)
+    output
+
+let workingItemsPerMinute (speed: int): int =
+    let floatRatePerHour = floor ((productionRatePerHour speed)/60.0)
+    int floatRatePerHour
+
+let w = workingItemsPerMinute 6
+printfn "%i" w
