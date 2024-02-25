7e2433a4dd197b246fe2ba10630fe84b66c1b0e2
Felipe <fvhmanhaes@gmail.com>
2024-02-25 00:47:02 -0300

    code almost finished, but 4 of 25  tests still need to be passed

--- MyFSharpApp/obj/Debug/net8.0/MyFSharpApp.AssemblyInfo.fs
+++ MyFSharpApp/obj/Debug/net8.0/MyFSharpApp.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("MyFSharpApp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afc14ff28c2e90a1b904e98ea4a8f44ba5cf273")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84f276d17acb830bf48ea490cd5ecb43df33162")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("MyFSharpApp")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("MyFSharpApp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
