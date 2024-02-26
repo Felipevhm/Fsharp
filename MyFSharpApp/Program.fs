f1c4c173f7b22ac3dad3afd1e74ea354a3da864b
Felipe <fvhmanhaes@gmail.com>
2024-02-26 16:31:45 -0300

    ex4_record_igrejas updated / new functionality of reporting churches by people that go there

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -1,13 +1,100 @@
-﻿let inverteLista lista =
-    let rec auxiliar acc lista =
-        match lista with
-        | [] -> acc
-        | cabeca :: cauda -> auxiliar (cabeca :: acc) cauda
-    auxiliar [] lista
+﻿type Church =
+    { Name : string
+      Denomination : string
+      Address : string
+      Capacity : int 
+      CommunityMembers : string []
+      }
 
-// Exemplo de uso
-let listaOriginal = [1; 2; 3; 4; 5]
-let listaInvertida = inverteLista listaOriginal
+type ChurchStatus =
+    | Open
+    | Closed
 
-printfn "Lista Original: %A" listaOriginal
-printfn "Lista Invertida: %A" listaInvertida
+let churchesData =
+    [ 
+      { Name = "Catedral Metropolitana de Florianópolis"
+        Denomination = "Católica"
+        Address = "Praça XV de Novembro, Centro"
+        Capacity = 500 
+        CommunityMembers = [|"Daniel";"Maria"|]
+        }
+        ,
+      Open;
+
+      { Name = "Moteiro Santo Ivo"
+        Denomination = "Católica"
+        Address = "R. Delminda Silveira, Agronômica"
+        Capacity = 80 
+        CommunityMembers = [|"Juarez";"Erica";"Daniel"|]
+        }
+        ,
+      Open;
+
+      { Name = "Igreja da Trindade"
+        Denomination = "Católica"
+        Address = "Pça. Santos Dumont, Trindade"
+        Capacity = 600 
+        CommunityMembers = [|"Maria";"Erica"|]
+        }
+        ,
+      Open;
+
+    ]
+
+printfn "%A\n\n" churchesData
+
+let peopleSearch (churchesData:(Church * ChurchStatus) list): string list  =
+    churchesData
+    |> List.collect (fun (church, status) -> Array.toList church.CommunityMembers)
+    |> Set.ofList
+    |> Set.toList
+
+let returnMembers = (peopleSearch churchesData)
+
+printfn "%A\n" returnMembers
+
+
+// ----
+//1)
+let isMember (name: string) (churchesData:(Church * ChurchStatus) list): bool list  =
+    churchesData
+    |> List.map (fun (church, status) -> Array.contains name church.CommunityMembers)
+
+//let checkMember = (isMember "Maria" churchesData)
+//printfn "%A\n" checkMember
+
+//2)
+let getChurchNames (churchesData:(Church * ChurchStatus) list): string list  =
+    churchesData
+    |> List.map (fun (church, status) -> church.Name)
+
+// let churchNames = (getChurchNames churchesData)
+
+//printfn "%A\n" churchNames
+//3)
+let filterChurchNames (churchNames: string list) (checkMember: bool list): string list =
+    List.zip churchNames checkMember
+    |> List.filter snd
+    |> List.map fst
+
+//let filteredChurchNames = filterChurchNames churchNames checkMember
+
+//printfn "%A\n" filteredChurchNames
+//----------------------------------------------------------------  
+// let checkMember = (isMember "Maria" churchesData)
+
+// printfn "%A\n" checkMember
+
+let personChurches (name: string) (churchesData:(Church * ChurchStatus) list) = 
+
+    let checkMember = (isMember name churchesData)
+    let churchNames = (getChurchNames churchesData)
+    let filteredChurchNames = filterChurchNames churchNames checkMember
+    filteredChurchNames
+   
+
+for name in returnMembers do
+  printfn "%s:\n" name
+  let testLastFunction = personChurches name churchesData
+
+  printfn "%A\n" testLastFunction
