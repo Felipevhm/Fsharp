ec0110a491529abf3c31849a91df4de824020ad4
Felipe <fvhmanhaes@gmail.com>
2024-03-04 15:58:11 -0300

    added homepage with Hello world message and counter

--- Counter/src/Counter.Server/Index.fs
+++ Counter/src/Counter.Server/Index.fs
@@ -16,20 +16,20 @@ let page = doctypeHtml {
         link { attr.rel "stylesheet"; attr.href "Counter.Client.styles.css" }
     }
     body {
-        nav {
-            attr.``class`` "navbar is-dark"
-            "role" => "navigation"
-            attr.aria "label" "main navigation"
-            div {
-                attr.``class`` "navbar-brand"
-                a {
-                    attr.``class`` "navbar-item has-text-weight-bold is-size-5"
-                    attr.href "https://fsbolero.io"
-                    img { attr.style "height:40px"; attr.src "https://github.com/fsbolero/website/raw/master/src/Website/img/wasm-fsharp.png" }
-                    "  Bolero"
-                }
-            }
-        }
+        // nav {
+        //     attr.``class`` "navbar is-dark"
+        //     "role" => "navigation"
+        //     attr.aria "label" "main navigation"
+        //     div {
+        //         attr.``class`` "navbar-brand"
+        //         a {
+        //             attr.``class`` "navbar-item has-text-weight-bold is-size-5"
+        //             attr.href "https://fsbolero.io"
+        //             img { attr.style "height:40px"; attr.src "https://github.com/fsbolero/website/raw/master/src/Website/img/wasm-fsharp.png" }
+        //             "  Bolero"
+        //         }
+        //     }
+        // }
         div { attr.id "main"; comp<Client.Main.MyApp> }
         boleroScript
     }
