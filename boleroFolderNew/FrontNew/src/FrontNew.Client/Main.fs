8aab09409e17c372516d15eaf924372c983283ec
Felipe <fvhmanhaes@gmail.com>
2024-03-08 14:48:00 -0300

    Update some code

--- boleroFolderNew/FrontNew/src/FrontNew.Client/Main.fs
+++ boleroFolderNew/FrontNew/src/FrontNew.Client/Main.fs
@@ -61,8 +61,7 @@ let update (http: HttpClient) message model =
     | Decrement ->
         { model with counter = model.counter - 1 }, Cmd.none
     | SetCounter value ->
-        { model with counter = value }, Cmd.none
-
+        { model with counter = value }, Cmd.none //
     | GetBooks ->
         let getBooks() = http.GetFromJsonAsync<Book[]>("/books.json")
         let cmd = Cmd.OfTask.either getBooks () GotBooks Error
@@ -107,26 +106,11 @@ let dataPage model dispatch =
         .Elt()
 
 
-// let menuItem (model: Model) (page: Page) (text: string) =
-//     Main.MenuItem()
-//         .Active(if model.page = page then "is-active" else "")
-//         .Url(router.Link page)
-//         .Text(text)
-//         .Elt()
-
 let view model dispatch =
     Main()
-        // .Menu(concat {
-        //     menuItem model Home "Home"
-        //     // menuItem model Counter "Counter"
-        //     // menuItem model Data "Download data"
-        // })
         .Body(
             cond model.page <| function
             | Home -> counterPage model dispatch
-            // | Counter -> homePage model dispatch
-            // | Data ->
-            //     dataPage model dispatch
         )
         .Error(
             cond model.error <| function
